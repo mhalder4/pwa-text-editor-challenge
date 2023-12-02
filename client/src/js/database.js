0eecd9fb69c5e0ae894cc8b515ea46d5e5d127f3
unknown <mhalder9555@gmail.com>
2023-12-02 17:26:21 -0600

    Finished database.js file.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,7 @@
 import { openDB } from 'idb';
 
+const DB_NAME = "jate";
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +15,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  const textDb = await openDB(DB_NAME, 1);
+
+  const tx = textDb.transaction(DB_NAME, 'readwrite');
+
+  const store = tx.objectStore(DB_NAME);
+
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  const textDb = await openDB(DB_NAME, 1);
+
+  const tx = textDb.transaction(DB_NAME, 'readonly');
+
+  const store = tx.objectStore(DB_NAME);
+
+
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+
+  return result?.value;
+};
 
 initdb();
